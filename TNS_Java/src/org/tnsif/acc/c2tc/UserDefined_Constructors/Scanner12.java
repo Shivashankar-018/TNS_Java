90b3504a2f49ee3f643af842239c461bb5c94b90
bshiv <bshiv@Shiva>
2026-08-04 13:02:24 +0530

    First Commit

---
+++ TNS_Java/src/org/tnsif/acc/c2tc/UserDefined_Constructors/Scanner12.java
@@ -0,0 +1,13 @@
+package org.tnsif.acc.c2tc.UserDefined_Constructors;
+import java.util.Scanner;
+public class Scanner12 {
+
+	public static void main(String[] args) {
+		Scanner dc=new Scanner(System.in);
+		System.out.println("Entet The String:");
+		char cc=dc.next().charAt(3);
+		System.out.println("Fourth Character :"+cc);
+
+	}
+
+}
---
+++ TNS_Java/src/org/tnsif/acc/c2tc/UserDefined_Constructors/ScannerClass.java
@@ -0,0 +1,13 @@
+package org.tnsif.acc.c2tc.UserDefined_Constructors;
+import java.util.Scanner;
+public class ScannerClass {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter The Name:");
+		       String name=sc.nextLine();
+		       System.out.println(name);
+		
+
+	}
+
+}
