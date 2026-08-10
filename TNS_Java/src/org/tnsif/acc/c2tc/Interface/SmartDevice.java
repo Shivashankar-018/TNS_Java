bcf4662483b6619907df4477914864f8a75aee2f
bshiv <bshiv@Shiva>
2026-08-10 12:51:06 +0530

    First commit

---
+++ TNS_Java/src/org/tnsif/acc/c2tc/Interface/SmartDevice.java
@@ -0,0 +1,12 @@
+package org.tnsif.acc.c2tc.Interface;
+
+public interface SmartDevice {
+
+	
+
+void turnOn();
+void turnOff();
+void getStatus();
+
+}
+
--- TNS_Java/src/org/tnsif/acc/c2tc/MethodOverRiding_Super_this_instanceof/package-info.java
+++
@@ -1,1 +0,0 @@
-package org.tnsif.acc.c2tc.MethodOverRiding_Super_this_instanceof;
